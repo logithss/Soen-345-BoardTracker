6fb483a59e68c2724346a6ab01d1fd0db2ee7773
Merouane Issad <merouaneissad@gmail.com>
2025-04-12 08:31:09 -0400

    + finalized tracking + added shortcut menu

--- app.py
+++ app.py
@@ -1,40 +1,129 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 import pywinstyles
+from pynput import keyboard
+from pynput.keyboard import Key, Controller
+import random
+from functools import partial
+import subprocess
+import os
 
-window = ctk.CTk()
-window.geometry('600x400')
-window.wm_attributes("-topmost", True)
-#window.wm_attributes("-disabled", True)
-window.config(bg = '#000001')
-window.wm_attributes("-transparentcolor", "#000001")
+global myKeyboard
+myKeyboard = Controller()
 
 global buttonY, buttonYDir, animating
 buttonY = 0.1
 buttonYDir = 1
 animating = False
-def animateUp():
-    global buttonY
-    
-    print(buttonY)
-    if (buttonY-100) <= 0.0001:
-        buttonY = 1
-        print("here: " + str(buttonY))
-        roundedbutton.place(relx=0.5, y=buttonY, anchor='s')
+
+global buttons, buttonImages
+buttons=[]
+buttonImages = ["buttonKeyboard.png","buttonCalculator.png","buttonBrush.png","buttonEraser.png","buttonExplorer.png","buttonCancel.png"]
+global buttonsPos, buttonsShown
+buttonsShown = True
+buttonsPos=[(0.23, 0.87), (0.35, 0.8), (0.5, 0.77), (0.65, 0.8), (0.77, 0.87), (0.5, 0.90)]
+
+window = ctk.CTk()
+
+
+window.geometry('1020x768+1920+0')
+window.overrideredirect(True)
+#indow.wm_attributes('-fullscreen','true')
+window.wm_attributes("-topmost", True)
+#window.wm_attributes("-disabled", True)
+window.config(bg = '#000001')
+window.wm_attributes("-transparentcolor", "#000001")
+
+def getButtonPos(index, time):
+    originPos = (0.5, 1.1)
+    selectedPos = buttonsPos[index]
+
+    return (originPos[0] + (selectedPos[0]-originPos[0]) * time, originPos[1] + (selectedPos[1]-originPos[1]) * time )
+
+def on_press(key, injected):
+    if str(key) == "'q'":
+        window.destroy()
+    if str(key) == "'b'":
+        startAnimUp()
+
+global buttonTime
+buttonTime = 0
+global buttonTimeDelta
+buttonTimeDelta = 0
+
+def startAnimUp():
+    global buttonTime, buttonTimeDelta, buttonsShown
+    if buttonTimeDelta == 0 and buttonsShown == False:
+        buttonTime = 0
+        buttonTimeDelta = 0.04
+        buttonsShown = True
+        animateButton()
+
+def startAnimDown():
+    global buttonTime, buttonTimeDelta, buttonsShown
+    if buttonTimeDelta == 0 and buttonsShown == True:
+        buttonTime = 1
+        buttonTimeDelta = -0.04
+        buttonsShown = False
+        animateButton()
+
+def animateButton():
+    global buttonTime, buttonTimeDelta
+    if(buttonTime > 1 or buttonTime < 0):
+        buttonTimeDelta = 0
         return
+    
+    for i in range(len(buttons)):
+        actualTime = buttonTime * (1 + buttonTimeDelta *i)
+        if actualTime > 1:
+            actualTime = 1
+        if actualTime < 0:
+            actualTime = 0
+        pos = getButtonPos(i, actualTime)
+        buttons[i].place(relx=pos[0], rely=pos[1], anchor='center')
+    
+    buttonTime += buttonTimeDelta
+    window.after(10, animateButton)
 
-    buttonY -= 5
-    roundedbutton.place(relx=0.5, y=buttonY, anchor='s')
-    window.after(200, animateUp)
+def buttonCommand(command):
+    global buttonTime, buttonTimeDelta, myKeyboard
+    if(buttonTime < 0.9 and buttonTime > 0.1):
+        return
+    if str(command) == '0':
+        freevkPath = 'C:\\Users\\Yasser\\Documents\\Soen357\\assets\\FreeVK.exe'
+        print(freevkPath)
+        subprocess.call([freevkPath])
+    elif str(command) == '1':
+        subprocess.call(['c:\windows\system32\calc.exe'])
+    elif str(command) == '2':
+        window.focus_set()
+        myKeyboard.press('b')
+        myKeyboard.release('b')
+    elif str(command) == '3':
+        window.focus_set()
+        myKeyboard.press('e')
+        myKeyboard.release('e')
+    elif str(command) == '4':
+        subprocess.call(['C:\windows/explorer.exe'])
+    elif str(command) == '5':
+        print("hide")
+    else:
+        return
+    startAnimDown()
 
-def click():
-    animateUp()
 
-img = ctk.CTkImage(Image.open("button.png"),size=[200, 200])
-roundedbutton = ctk.CTkButton(window, text="", image=img, fg_color='transparent', 
-                              bg_color="#000001", width = 200, height = 200, 
-                              border_width=0, hover = None, command=click, anchor='sw')
-roundedbutton.place(relx=0.5, y=200, anchor='s')
-pywinstyles.set_opacity(roundedbutton, color="#000001")
+for i in range(6):
+    buttonSize = [100, 100] if i!=5 else [80, 80]
+    img = ctk.CTkImage(Image.open("assets/"+buttonImages[i]),size=buttonSize)
+    action_with_arg = partial(buttonCommand, i)
+    button = ctk.CTkButton(window, text="", image=img, fg_color='transparent', 
+                                bg_color="#000001", width = buttonSize[0], height = buttonSize[1],
+                                border_width=0, hover = None, command= action_with_arg, anchor='center')
+    buttonPos = getButtonPos(i, 1)
+    button.place(relx=buttonPos[0], rely=buttonPos[1], anchor='center')
+    buttons.append(button)
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 
 window.mainloop()
--- cvTest2.py
+++ cvTest2.py
@@ -1,18 +1,28 @@
 import numpy as np
 import cv2
 import win32api, win32con
+from pynput import keyboard
+from pynput.mouse import Button, Controller
+from threading import Thread
+from time import sleep
+
+mouse = Controller()
 
 global pointerPos
 global rawUnitPos
 global controlMouse
-controlMouse = False
+global running
+pointerPos = None
+rawUnitPos = None
+running = True
+controlMouse = True
 
 global matrix
 pts1=np.float32([[0,0],[1,0],[1,1],[0,1]]) 
-pts2=np.float32([[184, 90],[393, 60],[395, 295],[191, 301]])
+pts2=np.float32([[207, 117],[429, 106],[433, 333],[203, 341]])
 matrix=cv2.getPerspectiveTransform(pts2, pts1)
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(2)
 
 def getPointerPosition():
     global pointerPos
@@ -23,19 +33,15 @@ def getPointerPosition():
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    mask1 = cv2.inRange(hsv, (170, 128, 128), (180, 255, 255)) # pinkish red
-    mask2 = cv2.inRange(hsv, (  0, 128, 128), ( 10, 255, 255)) # yellowish red
+    #mask1 = cv2.inRange(hsv, (170, 128, 128), (180, 255, 255)) # pinkish red
+    #mask2 = cv2.inRange(hsv, (  0, 128, 128), ( 10, 255, 255)) # yellowish red
 
-    maskFinal = mask1+mask2
+    #maskFinal = mask1+mask2
 
     # set my output img to zero everywhere except my mask
-    output_img = frame.copy()
+    #output_img = frame.copy()
 
-    ret,thresh1 = cv2.threshold(gray,240,255,cv2.THRESH_BINARY)
-    ret,thresh2 = cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV)
-    ret,thresh3 = cv2.threshold(gray,127,255,cv2.THRESH_TRUNC)
-    ret,thresh4 = cv2.threshold(gray,240,255,cv2.THRESH_TOZERO)
-    ret,thresh5 = cv2.threshold(gray,127,255,cv2.THRESH_TOZERO_INV)
+    ret,thresh4 = cv2.threshold(gray,230,255,cv2.THRESH_TOZERO)
     
     kernel = np.ones((3,3),np.uint8)
     dilated = cv2.dilate(thresh4, kernel, iterations=1)
@@ -45,11 +51,12 @@ def getPointerPosition():
 
     if len(contours) > 0:
         contourLength = cv2.arcLength(contours[0], True)
-        if contourLength < 35:
+        if contourLength < 50:
             pointerPos = (int(contours[0][0][0][0]), int(contours[0][0][0][1]))
             pointerCoordinateText = str(pointerPos[0]) + ", " + str(pointerPos[1])
             cv2.putText(frame, pointerCoordinateText, (200,40), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1,)
             cv2.putText(frame, str(contourLength), (200,80), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1,)
+            print(pointerCoordinateText)
         else:
            pointerPos = None
     else:
@@ -59,15 +66,17 @@ def getPointerPosition():
     #if cv2.countNonZero(dilated) == 0:
     #    cv2.rectangle(frame,(0,0),(200,200),(0,255,0),3)
     cv2.imshow('frame',frame)
-    cv2.imshow('gray',gray)
+    #cv2.imshow('gray',gray)
     cv2.imshow('hsv',hsv)
-    cv2.imshow('maskFinal',maskFinal)
-    cv2.imshow('thresh1',thresh1)
-    cv2.imshow('thresh4',dilated)
+    #cv2.imshow('maskFinal',maskFinal)
+    cv2.imshow('thresh',dilated)
 
 def performPointerMapping():
     global pointerPos
     global rawUnitPos
+    if pointerPos == None:
+        rawUnitPos = None
+        return
     p=pointerPos
     px = (matrix[0][0]*p[0] + matrix[0][1]*p[1] + matrix[0][2]) / ((matrix[2][0]*p[0] + matrix[2][1]*p[1] + matrix[2][2]))
     py = (matrix[1][0]*p[0] + matrix[1][1]*p[1] + matrix[1][2]) / ((matrix[2][0]*p[0] + matrix[2][1]*p[1] + matrix[2][2]))
@@ -81,26 +90,83 @@ def performPointerMapping():
         cv2.circle(perspResult,(10, 10), 5, (0,255,0), 5)
     else:
         cv2.circle(perspResult,(10, 10), 5, (0,0,170), 5)
-    cv2.imshow('perspResult',perspResult)
+    #cv2.imshow('perspResult',perspResult)
 
 def handleMousePosition():
-    global controlMouse
-    global rawUnitPos
-    if controlMouse:
-        win32api.SetCursorPos((int(1920+rawUnitPos[0]*800), int(rawUnitPos[1]*600)))
-while(True):
-    if cv2.waitKey(1) & 0xFF == ord('r'):
-        print(str(pointerPos[0]) + ", " + str(pointerPos[1]))
+    while(running):
+        global mouseInThreashold
+        global pointerPos
+        global controlMouse
+        global rawUnitPos
+        global isHeld
+        if controlMouse and pointerPos != None and rawUnitPos != None:
+            absolutePointer = (int(1920+rawUnitPos[0]*1024), int(rawUnitPos[1]*768))
+            mouseDelta = (int( (absolutePointer[0]-mouse.position[0]) ), int( (absolutePointer[1]-mouse.position[1]) ) )
+
+            if not mouseInThreashold:
+                targetMousePos = smooth_mouse(mouse.position, absolutePointer, 0.1)
+                mouse.position = targetMousePos
+                if abs(mouseDelta[0]) < 2 or abs(mouseDelta[1]) < 2:
+                    mouseInThreashold = True
+            else:
+                if abs(mouseDelta[0]) > 10 or abs(mouseDelta[1]) > 10:
+                    mouseInThreashold = False
+
+
+def smooth_mouse(current_pos, target_pos, smoothing_factor):
+    x = current_pos[0] + (target_pos[0] - current_pos[0]) * smoothing_factor
+    y = current_pos[1] + (target_pos[1] - current_pos[1]) * smoothing_factor
+    return (x, y)
+
+def handleMouseClick():
+    mouse.click(Button.left, 1)
+
+global mouseInThreashold
+mouseInThreashold = False
+
+global isHeld
+isHeld = False
+def handleMouseHold():
+    global isHeld
+    if isHeld:
+        mouse.release(Button.left)
+    else:
+        mouse.press(Button.left)
+    
+    isHeld = not isHeld
+
+
+def on_press(key, injected):
+    if key == keyboard.Key.media_volume_up:
+        handleMouseClick()  
+    elif key == keyboard.Key.media_volume_down:
+        handleMouseHold()   
+    elif str(key) == "'q'":
+        global running
+        running = False
+
+def on_release(key, injected):
+    print("no")
+
+listener = keyboard.Listener(
+     on_press=on_press,
+        on_release=on_release)
+listener.start()
+
+mousePositionThread = Thread(target = handleMousePosition)
+mousePositionThread.start()
+
+while(running):
     if cv2.waitKey(1) & 0xFF == ord('1'):
         controlMouse = True
     if cv2.waitKey(1) & 0xFF == ord('2'):
         controlMouse = False
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
     getPointerPosition()
     if pointerPos != None:
         performPointerMapping()
-        handleMousePosition()
+        #handleMousePosition()
+
+mousePositionThread.join()
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
